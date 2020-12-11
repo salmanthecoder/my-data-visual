d7c9a05bec6f51420c33bc9aabfc26fb9c8b0273
“salmanthecoder” <rahilsalman@gmail.com>
2020-12-11 09:40:10 +0100

    added components, services

---
+++ src/app/my-tree/my-tree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-tree',
+  templateUrl: './my-tree.component.html',
+  styleUrls: ['./my-tree.component.scss']
+})
+export class MyTreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

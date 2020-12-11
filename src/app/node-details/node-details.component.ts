95e150fcb565e3240ff9812b8a1f6fdb3ba6a68e
“salmanthecoder” <rahilsalman@gmail.com>
2020-12-11 09:59:42 +0100

    implementation ofpopup

--- src/app/node-details/node-details.component.ts
+++ src/app/node-details/node-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-node-details',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./node-details.component.scss']
 })
 export class NodeDetailsComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  description: string;
+  constructor(public dialogRef: MatDialogRef<NodeDetailsComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    this.name = this.data?.name;
+    this.description = this.data?.description;
+    this.dialogRef.updatePosition({ top: `30px`,
+    right: `40px`});
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
